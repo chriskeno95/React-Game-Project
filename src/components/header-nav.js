bed4233c00fe56677f4fa67626fc56470e865a74
chris kennedy <chris-kennedy09@hotmail.com>
2023-03-24 18:36:36 +0000

    added table structure as a highscore-data component and populated it with data. i also have added css styling for this

--- src/components/header-nav.js
+++ src/components/header-nav.js
@@ -2,14 +2,12 @@ import { ReactComponent as Logo } from "../Design-Materials/icons-etc/SVG/Logo.s
 import React, { useState } from "react";
 import Navbar from 'react-bootstrap/Navbar';
 import Modal from 'react-bootstrap/Modal';
+import BasicTable from "./highscore-data";
 
 function Header() {
   const [showModal, setShowModal] = useState(false);
 
   const handleModal = () => setShowModal(!showModal);
-  const handleClear = () => {
-    {/* we will add the rules here to clear the highscores */}
-  };
 
   return (
     <div>
@@ -29,10 +27,10 @@ function Header() {
           <Modal.Title className="highscores-title ">HIGHSCORES</Modal.Title>
         </Modal.Header>
         <Modal.Body className="custom-modal-body">
-          {/* Add our highscores content here */}
+          <BasicTable></BasicTable>
         </Modal.Body>
         <Modal.Footer className="custom-modal-footer">
-          <button className="modal-btn" onClick={handleClear}>CLEAR</button>
+          <button className="modal-btn">CLEAR</button>
           <button className="modal-btn" onClick={handleModal}>CLOSE</button>
         </Modal.Footer>
       </Modal>
